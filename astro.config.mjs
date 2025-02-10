300c31c2a103b235255c09f002431854ac2696a0
Bazmati <basilemalin@gmx.fr>
2025-02-10 10:11:02 +0100

    Correct site URL casing in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Bazmati.github.io',
+  site: 'https://bazmati.github.io',
   base: 'cv',
   trailingSlash: "always",
   integrations: [tailwind()],
