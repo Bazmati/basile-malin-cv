b1c5dd2af9f1d268a751af1776676d07fbef04e8
Bazmati <103054566+Bazmati@users.noreply.github.com>
2025-07-18 12:19:28 +0200

    modif

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bazmati.github.io',
-  base: 'cv',
+  base: 'basile-malin-cv',
   trailingSlash: "always",
   integrations: [tailwind()],
 });
